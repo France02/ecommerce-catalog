93f12c1e2270a3f1d1d177d58f0adf4c91adcfd0
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-09-20 16:22:31 +0200

    feat: Add DELETE endpoint to remove a product

--- src/main/java/com/francescogolino/ecommerce_catalog/controller/ProductController.java
+++ src/main/java/com/francescogolino/ecommerce_catalog/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.francescogolino.ecommerce_catalog.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,24 +19,24 @@ import com.francescogolino.ecommerce_catalog.repository.ProductRepository;
 public class ProductController {
     private final ProductRepository productRepository;
 
-    public ProductController(ProductRepository productRepository){
+    public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @GetMapping
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
     @PostMapping
-    public Product createProduct(@RequestBody Product product){
+    public Product createProduct(@RequestBody Product product) {
         return productRepository.save(product);
     }
 
     @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
         Product product = productRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Prodotto non trovato" + id));
+                .orElseThrow(() -> new RuntimeException("Prodotto non trovato" + id));
         product.setName(productDetails.getName());
         product.setDescription(productDetails.getDescription());
         product.setPrice(productDetails.getPrice());
@@ -43,6 +44,11 @@ public class ProductController {
 
         return productRepository.save(product);
 
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
     }
 
 }
