95835f4227d8bd4ccbfb2d3c06137d753c674a74
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-09-20 16:09:49 +0200

    feat: Add PUT endpoint to update a product

--- src/main/java/com/francescogolino/ecommerce_catalog/controller/ProductController.java
+++ src/main/java/com/francescogolino/ecommerce_catalog/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.francescogolino.ecommerce_catalog.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +31,18 @@ public class ProductController {
     public Product createProduct(@RequestBody Product product){
         return productRepository.save(product);
     }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
+        Product product = productRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Prodotto non trovato" + id));
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setPrice(productDetails.getPrice());
+        product.setImgUrl(productDetails.getImgUrl());
+
+        return productRepository.save(product);
+
+    }
+
 }
